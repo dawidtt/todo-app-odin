55143c30dd5f148bbef2b8d67215844ff4c23da1
dawidtt <dawid.gorszka@gmail.com>
2024-09-20 18:40:15 +0200

    Add styling to all section and add multiple icons as svg and style them

--- src/all-todos-section/all-todos-dom.js
+++ src/all-todos-section/all-todos-dom.js
@@ -28,8 +28,9 @@ export function generateAllTodos(todosArr) {
 
     const midTodoContainer = document.createElement("div");
     midTodoContainer.classList.add("mid-todo-container");
-    const expand = document.createElement("p");
-    expand.textContent = "expand";
+    let expand = document.createElement("svg");
+    expand.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144"/></svg>';
     midTodoContainer.appendChild(expand);
 
     const rightTodoContainer = document.createElement("div");
@@ -50,10 +51,12 @@ export function generateAllTodos(todosArr) {
 
     const edit = document.createElement("button");
     edit.classList.add("edit-btn");
-    edit.textContent = "Edit";
+    edit.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M459.94 53.25a16.06 16.06 0 00-23.22-.56L424.35 65a8 8 0 000 11.31l11.34 11.32a8 8 0 0011.34 0l12.06-12c6.1-6.09 6.67-16.01.85-22.38zM399.34 90L218.82 270.2a9 9 0 00-2.31 3.93L208.16 299a3.91 3.91 0 004.86 4.86l24.85-8.35a9 9 0 003.93-2.31L422 112.66a9 9 0 000-12.66l-9.95-10a9 9 0 00-12.71 0z"/></svg>';
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("delete-btn");
-    deleteBtn.textContent = "Delete";
+    deleteBtn.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M80 112h352"/><path d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
     rightTodoContainer.appendChild(dueDateContainer);
     rightTodoContainer.appendChild(priorityContainer);
     rightTodoContainer.appendChild(edit);
@@ -65,7 +68,7 @@ export function generateAllTodos(todosArr) {
     allWrapper.appendChild(todoContainer);
   }
   const bottomWrapper = document.createElement("div");
-  bottomWrapper.classList.add("bottom-wrapper");
+  bottomWrapper.classList.add("all-bottom-wrapper");
   const createTodo = document.createElement("button");
   createTodo.textContent = "Create ToDo";
   bottomWrapper.appendChild(createTodo);
