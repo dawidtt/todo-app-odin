c95e9f947dc4ed8c4e97abff764c7e4f964d6419
dawidtt <dawid.gorszka@gmail.com>
2024-09-26 02:19:54 +0200

    Create project logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./template.css";
 import * as allTodosDom from "./all-todos-section/all-todos-dom.js";
 import * as notesDom from "./notes-section/notes-dom.js";
 import * as switchTabs from "./switch-tabs.js";
+import * as projects from "./projects-modules/project-logic.js";
--- src/projects-modules/project-logic.js
+++ src/projects-modules/project-logic.js
@@ -1,1 +1,25 @@
-function createProject(projectName, todos)
+export { projects, createProject } from "./project-logic.js";
+function createProjectsArray() {
+  const projectsArray = [];
+  const addProject = (project) => {
+    projectsArray.push(project);
+  };
+  const deleteProject = (project) => {
+    const indexOfProject = projectsArray.indexOf(project);
+    projectsArray.splice(indexOfProject, 1);
+  };
+  return { projectsArray, addProject };
+}
+function createProject(projectName, todos = []) {
+  const addTodoToProject = (todo) => {
+    todos.push(todo);
+  };
+  const deleteTodoFromProject = (todo) => {
+    const indexOfTodo = todos.indexOf(todo);
+    todos.splice(indexOfTodo, 1);
+  };
+  return { projectName, todos, addTodoToProject, deleteTodoFromProject };
+}
+
+const projects = createProjectsArray();
+projects.addProject(createProject("Nazwa projektu"));
