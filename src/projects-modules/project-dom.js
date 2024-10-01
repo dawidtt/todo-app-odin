fac4a1c7dc44b15c5efd2c4cdddde48301ed9fd9
dawidtt <dawid.gorszka@gmail.com>
2024-10-01 19:00:57 +0200

    Add switch tabs functionality to projects

--- src/projects-modules/project-dom.js
+++ src/projects-modules/project-dom.js
@@ -2,14 +2,20 @@ import { markComplete } from "../todo-container/complete.js";
 import { generateTodoContainer } from "../todo-container/generate-todo-dom.js";
 import { todosArray } from "../todo-container/todo-logic.js";
 import { projects } from "./project-logic";
+import { switchTabs } from "../switch-tabs.js";
+import { renderNotesContainer } from "../notes-section/notes-dom.js";
+import { openModal } from "../all-todos-section/create-todo-modal.js";
 export * from "./project-dom.js";
 function generateMyProjects() {
   const myPojectsPath = document.querySelector("#projects-container-top");
   for (let i = 0; i < projects.getArray().length; i++) {
     const buttonProject = document.createElement("button");
+    buttonProject.addEventListener("click", switchTabs);
+
     buttonProject.addEventListener("click", () => {
       showSpecificProject(projects.getArray()[i]);
     });
+    buttonProject.addEventListener("click", renderNotesContainer);
     buttonProject.textContent = projects.getArray()[i].projectName;
     myPojectsPath.appendChild(buttonProject);
   }
@@ -17,10 +23,12 @@ function generateMyProjects() {
 generateMyProjects();
 
 function showSpecificProject(project) {
+  const mainPath = document.querySelector("main");
+  const allWrapper = document.createElement("div");
+  allWrapper.classList.add("all-wrapper");
   const projectHeadingText = project.projectName;
   // const todosArray = project.getTodosArray();
-  const allWraperPath = document.querySelector(".all-wrapper");
-  allWraperPath.innerHTML = "";
+
   const projectHeading = document.createElement("h2");
   projectHeading.textContent = projectHeadingText;
   const todosContainer = document.createElement("div");
@@ -32,8 +40,19 @@ function showSpecificProject(project) {
     }
   }
 
-  allWraperPath.appendChild(projectHeading);
-  allWraperPath.appendChild(todosContainer);
+  const bottomWrapper = document.createElement("div");
+  bottomWrapper.classList.add("all-bottom-wrapper");
+  const createTodo = document.createElement("button");
+  createTodo.textContent = "Create ToDo";
+  createTodo.classList.add("create-todo");
+  createTodo.addEventListener("click", openModal);
+
+  bottomWrapper.appendChild(createTodo);
+  allWrapper.appendChild(projectHeading);
+  allWrapper.appendChild(todosContainer);
+  mainPath.appendChild(allWrapper);
+  mainPath.appendChild(bottomWrapper);
+
   const todosContainerArray = Array.from(
     document.querySelectorAll(".todo-container")
   );
--- src/switch-tabs.js
+++ src/switch-tabs.js
@@ -1,16 +1,28 @@
 import { generateAllTodos } from "./all-todos-section/all-todos-dom";
 import { generateTodayTodos } from "./today-section/today-dom";
 import { todosArray } from "./todo-container/todo-logic";
+export { switchTabs };
 function switchTabs(e) {
   const mainPath = document.querySelector("main");
   const navBtnsNodeList = document.querySelectorAll("nav button");
   const navBtnsArray = Array.from(navBtnsNodeList);
+
+  const projectsNodeList = document.querySelectorAll(
+    "#projects-container-top button"
+  );
+  const projectsArray = Array.from(projectsNodeList);
   for (const navBtn of navBtnsArray) {
     navBtn.classList.remove("nav-section-checked");
   }
+
+  for (const projectBtn of projectsArray) {
+    projectBtn.classList.remove("nav-section-checked");
+  }
   mainPath.innerHTML = "";
   e.srcElement.classList.add("nav-section-checked");
+  console.log("halo");
 }
+
 const all = document.querySelector("#all-btn");
 const today = document.querySelector("#today-btn");
 const calendar = document.querySelector("#calendar-btn");
